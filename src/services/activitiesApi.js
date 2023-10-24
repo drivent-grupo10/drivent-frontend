26f27f4f7b7f798ca0da906feefbccd7a6dde693
nilteras <niltonrnascimento@hotmail.com>
2023-10-24 19:26:39 -0300

    feat: filter activities by date

---
+++ src/services/activitiesApi.js
@@ -0,0 +1,21 @@
+import api from './api';
+
+
+export async function getDays(token) {
+  const response = await api.get('/activities/', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
+
+export async function getActivitiesOfDay(date, token) {
+  console.log(date.startAt)
+  const response = await api.get(`/activities/${date.startAt}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
